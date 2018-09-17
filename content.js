60592390a82b01f73cd04e8ef3ff35e0bd520ca9
Ankit Gupta <ankit.g@pharmeasy.in>
2018-09-17 08:22:11 +0000

    add code in files types: .php,.js remove code in files types: .php,.js

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
 console.log("dvsem")
 console.log("udoya")
 console.log("fdxwm")
-console.log("jouji")
+console.log("rjkup")
--- content.php
+++ content.php
@@ -7,5 +7,5 @@
         echo "inifn";
         echo "hveod";
         echo "kmbxa";
-        echo "uvcwd";
+        echo "wvxuv";
 ?>
